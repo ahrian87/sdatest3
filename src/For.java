d1d0765a6bd77dde011a8880528b4366b38f8282
ahrian87 <maciej.zbydniewski@gmail.com>
2022-10-25 20:02:31 +0200

    pętla for z kotkami

---
+++ src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        String [] tableOfCats = new String[] {"Mruczek", "Kotlet", "Tofik", "Gówniak"};
+        for (int i = 0; i < tableOfCats.length; i++) {
+            System.out.println(tableOfCats[i]);
+        }
+    }
+}
