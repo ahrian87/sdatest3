4846d0acd0ca95f8393195ef1ec6dba5f4c65961
ahrian87 <maciej.zbydniewski@gmail.com>
2022-10-25 20:21:18 +0200

    rozne takie

---
+++ src/Dowhile.java
@@ -0,0 +1,5 @@
+public class Dowhile {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String [] tableOfCats = new String[] {"Mruczek", "Kotlet", "Tofik", "Gówniak"};
+        for (String cat: tableOfCats) {
+            System.out.println(cat);
+        }
+    }
+}
